7f259c3d984093ed78315a31d4a2bd8b967ce5c5
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-10-16 00:04:29 +0500

    Task5

--- Tyuiu.MilyutinND.Sprint2.Task5.V12.Test/DataServiceTest.cs
+++ Tyuiu.MilyutinND.Sprint2.Task5.V12.Test/DataServiceTest.cs
@@ -9,11 +9,13 @@ namespace Tyuiu.MilyutinND.Sprint2.Task5.V12.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
+
             int g = 2000;
             int m = 1;
             int n = 5;
             string res = ds.FindDateOfPreviousDay(g, m, n);
             string wait = Convert.ToString(5 + "." + 12 + "." + 1999);
+
             Assert.AreEqual(wait, res);
         }
     }
